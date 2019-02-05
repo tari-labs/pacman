1f42ed65fba7dceab9cd197019d6f7e6ddc98eda
CjS77 <CjS77@users.noreply.github.com>
2019-02-05 14:01:14 +0200

    Remove dead code/comments

--- src/boss.js
+++ src/boss.js
@@ -37,7 +37,6 @@ function dosScreen(ctx) {
     //var size = Math.floor(20.0 / renderScale);
     var size = 8.0;
     ctx.save();
-    // ctx.translate(0, 0);
     ctx.textAlign = "left";
     ctx.font = size + "px dos437,courier";
     ctx.strokeStyle = "#fff";
@@ -67,7 +66,6 @@ function dosScreen2(ctx) {
     ];
     var char_height = 7.0;
     ctx.save();
-    // ctx.translate(-10, 20);
     ctx.textAlign = "left";
     ctx.font = char_height + "px dos437,courier";
     ctx.strokeStyle = "#fff";
